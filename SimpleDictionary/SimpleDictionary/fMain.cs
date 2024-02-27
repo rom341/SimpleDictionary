ea1a6df09c28374c044e51c0298af5570a6062f0
rom341 <roman.murzik.antpnov@gmail.com>
2024-02-27 12:37:29 +0400

    added grid table for print a dictionary.

--- SimpleDictionary/SimpleDictionary/fMain.cs
+++ SimpleDictionary/SimpleDictionary/fMain.cs
@@ -13,6 +13,9 @@ namespace SimpleDictionary
 {
     public partial class fMain : Form
     {
+        int maxColumsCount = 3;
+        PanelManager panelManager = new PanelManager();
+        List<List<string>> dictionary = new List<List<string>>();
         public fMain()
         {
             InitializeComponent();
@@ -24,6 +27,67 @@ namespace SimpleDictionary
             {
                 StreamReader sr = new StreamReader(openFileDialog1.FileName);
                 string readedText = sr.ReadToEnd();
+                updateDictionaryTable(readedText);
+                updateDgvDictionaryTable();
+            }
+            
+        }
+        private void WriteToTxtFile(List<List<string>> data, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var record in data)
+                {
+                    string line = string.Join(" - ", record);
+                    writer.WriteLine(line);
+                }
+            }
+        }
+        private void updateDictionaryTable(string text)
+        {
+            List<string> lines = text.Split('\n').ToList();
+            foreach(string line in lines)
+            {
+                List<string> values = line.Split(new string[] { " - " }, StringSplitOptions.None).ToList();
+                for (int i = 0; i < values.Count; i++) 
+                {
+                    values[i] = values[i].Trim();
+                }
+                if (values.Count > maxColumsCount)
+                {
+                    maxColumsCount = values.Count;
+                }
+                dictionary.Add(values.ToList());                
+            }
+        }
+        private void updateDgvDictionaryTable()
+        {
+            dgvDictionaryTable.Columns.Clear();
+            for (int i = 0; i < maxColumsCount; i++)
+            {
+                dgvDictionaryTable.Columns.Add("Column" + i, "Column" + i);
+            }
+            foreach (List<string> line in dictionary)
+            {
+                dgvDictionaryTable.Rows.Add(line.ToArray());
+            }
+        }
+        private void fMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddColumn_Click(object sender, EventArgs e)
+        {
+            dgvDictionaryTable.Columns.Add(tbColumnName.Text, tbColumnName.Text);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                WriteToTxtFile(dictionary, saveFileDialog1.FileName);
             }
         }
     }
